ce7779defc620342672b4605a340421f40dc6777
lordrichard6 <paulolopesreizinho@gmail.com>
2021-10-21 18:19:01 +0200

    start navbar and header

---
+++ components/Header/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export default function Header() {
+    return (
+        <SectionContainer>
+            <h1>Header</h1>
+        </SectionContainer>
+    )
+}
+
+const SectionContainer = styled.div`
+  
+`
---
+++ components/Navbar/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export default function Navbar() {
+    return (
+        <NavbarContainer>
+            <h1>Navbar</h1>
+        </NavbarContainer>
+    )
+}
+
+const NavbarContainer = styled.div`
+  
+`
